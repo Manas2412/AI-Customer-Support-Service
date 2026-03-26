457f22d277e208718661391ac278b27ecb04dd63
Manas Sisodia <manas@192.168.1.12>
2026-03-26 12:06:11 +0530

    added amin.py file

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from ai-customer-support!")
+from extraction import EmailFetcher
+from support import AICustomerSupprt
+from dotenv import load_dotenv
+import asyncio
+
+async def ftech_and_process_emails(fetcher, ai_support):
+    while True:
+        print("Checking for the new email...")
+        new_emails = await fetcher.fetch_new_emails()
+        for email_message, sender_name, sender_addr in new_emails:
+            if sender_addr not in fetcher.whitelist:
+                print(
+                    f"Ignored email from: {sender_name} ({sender_addr}) = Not in whitelist"
+                )
+                continue
+
+            extracted_properties, evaluation_result = await ai_support.process_email(
+                email_message
+            )
+            print(f"Email from : {sender_name} ({sender_addr})")
+
+            subject = "AI Customer Service reply"
+            fetcher.send_email(sender_addr, subject, evaluation_result)
+
+        print("Sleeping for 10 seconds...")
+        await asyncio.sleep(10)
+
+
+
+async def main():
+    load_dotenv()
+    fetcher = EmailFetcher()
+    ai_support = AICustomerSupprt(ollama_model = "llama3")
+    await fetch_and_process_emails(fetcher, ai_support)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
